4a82c81582f7ee81e9c9ff301df2db1c91cde93e
Tetsuya Mori <monry84@gmail.com>
2020-02-03 20:46:29 +0900

    :sparkles: Change interfaces
    * 型制約でのオーバーロード切り替えは無理なので、メソッド自体を分ける

--- Assets/Scripts/Application/Interface/IKeyValueStore.cs
+++ Assets/Scripts/Application/Interface/IKeyValueStore.cs
@@ -1,4 +1,5 @@
 using System;
+using CAFU.KeyValueStore.Application.Utility;
 using JetBrains.Annotations;
 using UniRx.Async;
 
@@ -7,8 +8,10 @@ namespace CAFU.KeyValueStore.Application.Interface
     [PublicAPI]
     public interface IKeyValueStore
     {
-        UniTask<T> Get<T>(string key, T defaultValue = default, Func<string, T> deserializeCallback = default);
-        UniTask Set<T>(string key, T value, Func<T, string> serializeCallback = default);
+        UniTask<T> GetValueType<T>(string key, T defaultValue = default, Func<string, ReferenceWrapper<T>> deserializeCallback = default) where T : struct;
+        UniTask<T> GetReferenceType<T>(string key, T defaultValue = default, Func<string, T> deserializeCallback = default) where T : class;
+        UniTask SetValueType<T>(string key, T value, Func<ReferenceWrapper<T>, string> serializeCallback = default) where T : struct;
+        UniTask SetReferenceType<T>(string key, T value, Func<T, string> serializeCallback = default) where T : class;
         UniTask<bool> Has(string key);
     }
 }
