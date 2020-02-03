df9e88344f269043400301495446392ba36d40b5
Tetsuya Mori <monry84@gmail.com>
2020-02-03 20:48:33 +0900

    :sparkles: Follow interfaces changes
    * インタフェースの変更に追従
    * ReferenceType との相互変換は注意する

--- Assets/Scripts/Data/DataStore/Implement/PlayerPrefs.cs
+++ Assets/Scripts/Data/DataStore/Implement/PlayerPrefs.cs
@@ -1,4 +1,5 @@
 using System;
+using CAFU.KeyValueStore.Application.Utility;
 using CAFU.KeyValueStore.Data.Repository.Interface.DataStore;
 using JetBrains.Annotations;
 using UniRx.Async;
@@ -22,30 +23,37 @@ namespace CAFU.KeyValueStore.Data.DataStore.Implement
             }
 
             T result;
-            if (typeof(T) == typeof(bool))
+            if (typeof(T) == typeof(ReferenceWrapper<bool>))
             {
-                result = (T) (object) (UnityEngine.PlayerPrefs.GetInt(key, (bool)(object) defaultValue ? 1 : 0) == 1);
+                result = (T) (object) (UnityEngine.PlayerPrefs.GetInt(key, (defaultValue as ReferenceWrapper<bool>).Unwrap() ? 1 : 0) == 1).Wrap();
             }
-            else if (typeof(T) == typeof(int))
+            else if (typeof(T) == typeof(ReferenceWrapper<int>))
             {
-                result = (T) (object) UnityEngine.PlayerPrefs.GetInt(key, (int) (object) defaultValue);
+                result = (T) (object) UnityEngine.PlayerPrefs.GetInt(key, (defaultValue as ReferenceWrapper<int>).Unwrap()).Wrap();
             }
-            else if (typeof(T) == typeof(float))
+            else if (typeof(T) == typeof(ReferenceWrapper<float>))
             {
-                result = (T) (object) UnityEngine.PlayerPrefs.GetFloat(key, (float) (object) defaultValue);
+                result = (T) (object) UnityEngine.PlayerPrefs.GetFloat(key, (defaultValue as ReferenceWrapper<float>).Unwrap()).Wrap();
             }
             else if (typeof(T) == typeof(string))
             {
                 result = (T) (object) UnityEngine.PlayerPrefs.GetString(key, (string) (object) defaultValue);
             }
+            else if (deserializeCallback != default)
+            {
+                result = deserializeCallback(UnityEngine.PlayerPrefs.GetString(key));
+            }
             else
             {
-                if (deserializeCallback == default)
+                // Add `else if` when add Deserializer methods
+                if (typeof(T) == typeof(ReferenceWrapper<DateTime>))
+                {
+                    result = Deserializer.DateTime(UnityEngine.PlayerPrefs.GetString(key)) as T;
+                }
+                else
                 {
                     throw new ArgumentException($"Need third argument to get type of {typeof(T)}");
                 }
-
-                result = deserializeCallback(UnityEngine.PlayerPrefs.GetString(key));
             }
 
             return await UniTask.FromResult(result);
@@ -55,25 +63,35 @@ namespace CAFU.KeyValueStore.Data.DataStore.Implement
         {
             switch (value)
             {
-                case bool v:
+                case ReferenceWrapper<bool> v:
                     UnityEngine.PlayerPrefs.SetInt(key, v ? 1 : 0);
                     break;
-                case int v:
+                case ReferenceWrapper<int> v:
                     UnityEngine.PlayerPrefs.SetInt(key, v);
                     break;
-                case float v:
+                case ReferenceWrapper<float> v:
                     UnityEngine.PlayerPrefs.SetFloat(key, v);
                     break;
                 case string v:
                     UnityEngine.PlayerPrefs.SetString(key, v);
                     break;
                 default:
-                    if (serializeCallback == default)
+                    if (serializeCallback != default)
                     {
-                        throw new ArgumentException($"Need third argument to set type of {typeof(T)}");
+                        UnityEngine.PlayerPrefs.SetString(key, serializeCallback(value));
+                        break;
                     }
 
-                    UnityEngine.PlayerPrefs.SetString(key, serializeCallback(value));
+                    switch (value)
+                    {
+                        // Add `case` when add Serializer methods
+                        case ReferenceWrapper<DateTime> v:
+                            UnityEngine.PlayerPrefs.SetString(key, Serializer.DateTime(v));
+                            break;
+                        default:
+                            throw new ArgumentException($"Need third argument to set type of {typeof(T)}");
+                    }
+
                     break;
             }
 
