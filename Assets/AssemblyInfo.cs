3557e32b0e863aba8230e9ab45c0f162754674fc
Tetsuya Mori <monry84@gmail.com>
2020-02-07 15:16:19 +0900

    :clipboard: Setup assembly access to execute test

---
+++ Assets/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿#if UNITY_EDITOR
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("CAFU.KeyValueStore.Tests.Runtime")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+#endif
