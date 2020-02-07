d9d5c3e7118b6bafc288c69aee80f35110dbf079
Tetsuya Mori <monry84@gmail.com>
2020-02-07 17:16:07 +0900

    :white_check_mark: Write test cases for DataStore

---
+++ Assets/Tests/Runtime/Scripts/DataStore/Memory版.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using CAFU.KeyValueStore.Application.Installer;
+using CAFU.KeyValueStore.Application.Interface;
+using CAFU.KeyValueStore.Application.Master;
+using NUnit.Framework;
+using UniRx.Async;
+using UnityEngine.TestTools;
+using Zenject;
+
+namespace CAFU.KeyValueStore.DataStore
+{
+    public class Memory版 : ZenjectIntegrationTestFixture
+    {
+        [Inject] private IKeyValueStore KeyValueStore { get; }
+
+        private static string Key { get; } = "MemoryTest";
+
+        [SetUp]
+        public void Install()
+        {
+            PreInstall();
+            KeyValueStoreInstaller.Install(Container, DataStoreType.Memory);
+            PostInstall();
+
+            Container.Inject(this);
+        }
+
+        [UnityTest]
+        public IEnumerator 基本メソッド()
+        {
+            yield return KeyValueStore
+                .Has(Key)
+                .ToCoroutine(x => Assert.That(x, Is.False));
+            yield return KeyValueStore
+                .Get(Key, "DefaultValue")
+                .ToCoroutine(x => Assert.That(x, Is.EqualTo("DefaultValue")));
+            yield return KeyValueStore
+                .Set(Key, "NewValue")
+                .ToCoroutine();
+            yield return KeyValueStore
+                .Has(Key)
+                .ToCoroutine(x => Assert.That(x, Is.True));
+            yield return KeyValueStore
+                .Get<string>(Key)
+                .ToCoroutine(x => Assert.That(x, Is.EqualTo("NewValue")));
+        }
+    }
+}
---
+++ Assets/Tests/Runtime/Scripts/DataStore/PlayerPrefs版.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using CAFU.KeyValueStore.Application.Installer;
+using CAFU.KeyValueStore.Application.Interface;
+using CAFU.KeyValueStore.Application.Master;
+using NUnit.Framework;
+using UniRx.Async;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Zenject;
+
+namespace CAFU.KeyValueStore.DataStore
+{
+    public class PlayerPrefs版 : ZenjectIntegrationTestFixture
+    {
+        [Inject] private IKeyValueStore KeyValueStore { get; }
+
+        private static string Key { get; } = "PlayerPrefsTest";
+
+        [SetUp]
+        public void Install()
+        {
+            PreInstall();
+            KeyValueStoreInstaller.Install(Container, DataStoreType.PlayerPrefs);
+            PostInstall();
+
+            Container.Inject(this);
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            PlayerPrefs.DeleteAll();
+        }
+
+        [UnityTest]
+        public IEnumerator 基本メソッド()
+        {
+            yield return KeyValueStore
+                .Has(Key)
+                .ToCoroutine(x => Assert.That(x, Is.False));
+            yield return KeyValueStore
+                .Get(Key, "DefaultValue")
+                .ToCoroutine(x => Assert.That(x, Is.EqualTo("DefaultValue")));
+            yield return KeyValueStore
+                .Set(Key, "NewValue")
+                .ToCoroutine();
+            yield return KeyValueStore
+                .Has(Key)
+                .ToCoroutine(x => Assert.That(x, Is.True));
+            yield return KeyValueStore
+                .Get<string>(Key)
+                .ToCoroutine(x => Assert.That(x, Is.EqualTo("NewValue")));
+        }
+    }
+}
