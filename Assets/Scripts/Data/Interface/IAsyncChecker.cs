03884519295fd1ace189fdcc8f8e9600f4176b55
Tetsuya Mori <monry84@gmail.com>
2020-02-07 14:46:46 +0900

    :sparkles: Add CancecllationToken to awaitable methods

--- Assets/Scripts/Data/Interface/IAsyncChecker.cs
+++ Assets/Scripts/Data/Interface/IAsyncChecker.cs
@@ -1,9 +1,10 @@
+using System.Threading;
 using UniRx.Async;
 
 namespace CAFU.KeyValueStore.Data.Repository.Interface.DataStore
 {
     internal interface IAsyncChecker
     {
-        UniTask<bool> Has(string key);
+        UniTask<bool> Has(string key, CancellationToken cancellationToken = default);
     }
 }
--- Assets/Scripts/Data/Interface/IAsyncGetter.cs
+++ Assets/Scripts/Data/Interface/IAsyncGetter.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Threading;
 using UniRx.Async;
 
 namespace CAFU.KeyValueStore.Data.Repository.Interface.DataStore
 {
     internal interface IAsyncGetter
     {
-        UniTask<T> GetAsync<T>(string key, T defaultValue = default, Func<string, T> deserializeCallback = default);
+        UniTask<T> GetAsync<T>(string key, T defaultValue = default, Func<string, T> deserializeCallback = default, CancellationToken cancellationToken = default);
     }
 }
